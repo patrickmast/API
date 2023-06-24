bb25b5e15c80e42f7d79a044ca50d0153c9d2753
Patrick Mast <patrick@winfakt.com>
2023-06-24 14:40:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
+import random
+import string
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
 class Msg(BaseModel):
     msg: str
+
+# Function to generate random shortcode
+def generate_shortcode(length: int = 6) -> str:
+    shortcode = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    return shortcode
+
+# Dictionary to store shortcode to URL mappings
+url_mappings = {}
 
 
 @app.get("/")
@@ -25,3 +35,19 @@ async def demo_post(inp: Msg):
 @app.get("/path/{path_id}")
 async def demo_get_path_id(path_id: int):
     return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+
+@app.post("/shorten-url")
+async def shorten_url(url: str):
+    shortcode = generate_shortcode()
+    url_mappings[shortcode] = url
+    return {"shortcode": shortcode}
+
+
+@app.get("/{shortcode}")
+async def redirect_url(shortcode: str):
+    if shortcode not in url_mappings:
+        raise HTTPException(status_code=404, detail="Shortcode not found")
+
+    original_url = url_mappings[shortcode]
+    return {"url": original_url}
