43af8b25f9e88d6cba6e45b13728d00a53a40209
Patrick Mast <patrick@winfakt.com>
2023-06-24 15:30:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,27 +16,6 @@ def generate_shortcode(length: int = 6) -> str:
 # Dictionary to store shortcode to URL mappings
 url_mappings = {}
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
-
-
 @app.post("/shorten-url")
 async def shorten_url(url: str):
     shortcode = generate_shortcode()
